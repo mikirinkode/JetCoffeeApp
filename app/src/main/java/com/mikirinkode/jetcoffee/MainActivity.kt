edfc827d109799ca39c21917e872e8c6a40389a8
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-11-28 15:20:39 +0700

    add category section

--- app/src/main/java/com/mikirinkode/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/mikirinkode/jetcoffee/MainActivity.kt
@@ -5,21 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mikirinkode.jetcoffee.model.Category
+import com.mikirinkode.jetcoffee.model.dummyCategory
 import com.mikirinkode.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +42,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+        SectionText(title = "Mau ngopi apa hari ini?")
+        CategoryRow()
+    }
 }
 
 @Composable
@@ -75,7 +87,7 @@ fun SearchBarPreview() {
 
 @Composable
 fun Banner(modifier: Modifier = Modifier) {
-    Box (modifier = modifier){
+    Box(modifier = modifier) {
         Image(
             painter = painterResource(id = R.drawable.banner),
             contentDescription = "Banner Image",
@@ -94,7 +106,67 @@ fun BannerPreview() {
     }
 }
 
+@Composable
+fun CategoryItem(category: Category, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = category.imageCategory), contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(category.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(top = 16.dp, bottom = 8.dp)
+        )
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun CategoryItemPreview() {
+    JetCoffeeTheme {
+        CategoryItem(
+            category = Category(
+                R.drawable.icon_category_cappuccino,
+                R.string.category_cappuccino
+            ), modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun CategoryRow(modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+fun SectionText(title: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.h5.copy(
+            fontWeight = FontWeight.ExtraBold
+        ),
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
+}
 
 @Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
 @Composable
