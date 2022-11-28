261cf8c04ae64cc823e81ec67c9400b4152ffdce
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-11-28 14:30:38 +0700

    initial commit

---
+++ app/src/main/java/com/mikirinkode/jetcoffee/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mikirinkode.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mikirinkode.jetcoffee.model.Category
+import com.mikirinkode.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+}
+
+
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
+@Composable
+fun JetCoffeeAppPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
